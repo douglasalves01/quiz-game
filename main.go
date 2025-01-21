ff9d8a8e378be0ccc1226df1568f9ac903eb9f01
Douglas Alves <douglasmonttozo@hotmail.com.br>
2025-01-20 22:25:10 -0300

    configurando funcao init

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 type Question struct{
 	Text string
 	Options []string
@@ -9,7 +13,22 @@ type Question struct{
 type GameState struct{
 	Name string
 	Points string
+	Questions []Question
+}
+
+func (g *GameState) init(){
+	fmt.Println("Seja bem vindo ao jogo")
+	fmt.Println("Escreva o seu nome")
+	reader := bufio.NewReader(os.Stdin)
+
+	name, err := reader.ReadString('\n')
+
+	if err != nil{
+		panic("Erro ao ler a string")
+	}
+	g.Name = name
 }
 func main(){
-	fmt.Println("Olá")
+	game1 :=&GameState{}
+	game1.init()
 }
