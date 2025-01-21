54d7ce77b54b0b19ab6bcb306de97f485aff1ed1
Douglas Alves <douglasmonttozo@hotmail.com.br>
2025-01-20 22:14:20 -0300

    configurando estado inicial

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/douglasalves01/quiz-game
+
+go 1.23.5
---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+type Question struct{
+	Text string
+	Options []string
+	Answer int
+}
+type GameState struct{
+	Name string
+	Points string
+}
+func main(){
+	fmt.Println("Olá")
+}
